f0481c18101fb5c34e9f13a23c68fd3de2ffcd73
mikaf <webmail@handicapfreenet.com>
2024-11-01 21:56:33 +0900

    add:バリデーションファイル作成

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ContactRequest;
+use App\Http\Requests\CategoryRequest;
 
 class ContactController extends Controller
 {
-    public function index()
+    public function index(ContactRequest $request)
     {
         return view('index');
     }
---
+++ src/app/Http/Requests/ContactRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'gender' => ['required'],
+            'email' => ['required', 'email', 'max:255'],
+            'tell' => ['required', 'string', 'max:5', 'regex:/^[0-9]+$/'],
+            'address' => ['required', 'string', 'max:255'],
+            'building' => ['string', 'max:255'],
+            'detail' => ['required', 'string', 'max:120'],
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'first_name.required' => '名を入力してください',
+            'first_name.string' => '名を文字列で入力してください',
+            'first_name.max' => '名を255文字以下で入力してください',
+
+            'last_name.required' => '姓を入力してください',
+            'last_name.string' => '姓を文字列で入力してください',
+            'last_name.max' => '姓を255文字以下で入力してください',
+
+            'gender.required' => '性別を選択してください',
+
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレスはメール形式で入力してください',
+            'email.max' => 'メールアドレスを255文字以下で入力してください',
+
+            'tell.required' => '電話番号を入力してください',
+            'tell.regex' => '電話番号は5桁までの数字で入力してください',
+
+            'address.required' => '住所を入力してください',
+            'address.string' => '住所を文字列で入力してください',
+            'address.max' => '住所を255文字以下で入力してください',
+
+            'detail.required' => 'お問い合わせ内容を入力してください',
+            'detail.string' => 'お問い合わせ内容を文字列で入力してください',
+            'detail.max' => 'お問合せ内容は120文字以内で入力してください',
+        ];
+    }
+}
